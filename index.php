2c84678ba941d1976bc4415511c8b1de9c4cdd73
tmarke <ting_pangwee@hotmail.com>
2017-06-25 00:26:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
+
 <!DOCTYPE html>
 <html>
     <header>
-        <title>My webpage</title>
+        <title>My Webpage</title>
         <link rel="stylesheet" href="./Styles/styles.css"/>
     </header>
-
     <body>
-        <h1>hello world</h1>
-        <p>this is my webpage!</p>
+        <h1>T Marke's Webpage</h1>
+        <p>Welcome to my Webpage!</p>
         <img src="./Assets/photo.png"></img>
-          <h3>My name is fx</h3>
-          <i><a href="https://www.w3schools.com/html/">Visit my Facebook page</a></i>
+          <h3>My name is TMarke</h3>
+          <i><a href="https://www.instagram.com/rgdsmarke">Visit my Facebook page</a></i>
+  
+  
           <div class="warningMsg">This is a warning message</div>
     </body>
 </html>
