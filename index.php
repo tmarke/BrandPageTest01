04e827785fa4dd0c573393320d32ffff1957f963
tmarke <ting_pangwee@hotmail.com>
2017-06-25 00:08:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title>My webpage</title>
+        <link rel="stylesheet" href="./Styles/styles.css"/>
+    </header>
+
+    <body>
+        <h1>hello world</h1>
+        <p>this is my webpage!</p>
+        <img src="./Assets/photo.png"></img>
+          <h3>My name is fx</h3>
+          <i><a href="https://www.w3schools.com/html/">Visit my Facebook page</a></i>
+          <div class="warningMsg">This is a warning message</div>
+    </body>
+</html>
